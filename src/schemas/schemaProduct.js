c2a06d58067f1258d337ce36c85a7c44f3c34c61
HectorGarcia01 <hg2779948@gmail.com>
2024-09-08 23:19:25 -0600

    Creación del esquema de validación de productos

---
+++ src/schemas/schemaProduct.js
@@ -0,0 +1,33 @@
+const Joi = require('joi');
+
+const schemaProductCreate = Joi.object({
+  name: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(10)
+    .required(),
+  price: Joi.number()
+    .min(10)
+    .required()
+});
+
+const schemaProductUpdate = Joi.object({
+  name: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(10),
+  price: Joi.number()
+    .min(10)
+});
+
+const schemaGetProduct = Joi.object({
+  id: Joi.string()
+    .uuid()
+    .required()
+})
+
+module.exports = {
+  schemaProductCreate,
+  schemaProductUpdate,
+  schemaGetProduct
+}
