acc784537b95006a1b9141ec1e63f908ceb40492
HectorGarcia01 <hg2779948@gmail.com>
2024-09-08 23:17:57 -0600

    Agregando los endpoints de producto y usuario al servidor

--- src/server/index.js
+++ src/server/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const apiRouter = (app) => {
   const router = express.Router();
-  app.use('/api/v1', router);
   
+  app.use('/api/v1', router);
+
   router.use('/products', require('../routes/products.routes'));
   router.use('/users', require('../routes/users.routes'));
 };
