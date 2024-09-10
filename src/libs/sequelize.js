869a3c9507a6aadc694177f2f4bd5e2a9614fdf6
HectorGarcia01 <hg2779948@gmail.com>
2024-09-10 02:12:00 -0600

    Sincronización de los modelos

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 const { dbUser, dbPassword, dbHost, dbPort, dbName } = require('../config/config');
+const setupModels = require('../db/index');
 
 const USER = encodeURIComponent(dbUser);
 const PASSWORD = encodeURIComponent(dbPassword);
@@ -8,5 +9,8 @@ const URI = `postgres://${USER}:${PASSWORD}@${dbHost}:${dbPort}/${dbName}`;
 const sequelize = new Sequelize(URI, {
   dialect: 'postgres'
 });
+
+setupModels(sequelize);
+sequelize.sync();
 
 module.exports = sequelize;
