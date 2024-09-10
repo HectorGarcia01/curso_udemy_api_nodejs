7d9b02a110fb0b84f9eca6812ff6ba7ee7f225d1
HectorGarcia01 <hg2779948@gmail.com>
2024-09-10 00:31:55 -0600

    Implementación de pool para limitar la sobrecarga

--- src/libs/postgres.js
+++ src/libs/postgres.js
@@ -1,19 +1,13 @@
-const { Client } = require('pg');
-require('dotenv');
+const { Pool } = require('pg');
 
 const { HOST_POSTGRES, USER_POSTGRES, PASSWORD_POSTGRES, DB_POSTGRES, PORT_POSTGRES } = process.env;
 
-const getConnection = async () => {
-  const client = new Client({
+const pool = new Pool ({
     host: HOST_POSTGRES,
     port: PORT_POSTGRES,
     user: USER_POSTGRES,
     password: PASSWORD_POSTGRES,
     database: DB_POSTGRES
-  });
+});
 
-  await client.connect();
-  return client;
-};
-
-module.exports = getConnection;
+module.exports = pool;
