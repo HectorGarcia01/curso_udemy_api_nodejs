1b221060c22e81625c9fea555eb3af6e423f46ad
HectorGarcia01 <hg2779948@gmail.com>
2024-09-10 01:21:39 -0600

    Exportando las variables de entorno

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+  env: process.env.NODE_ENV || 'dev',
+  port: process.env.PORT || 3000,
+  dbUser: process.env.DB_POSTGRES,
+  dbPassword: process.env.PASSWORD_POSTGRES,
+  dbHost: process.env.HOST_POSTGRES,
+  dbName: process.env.DB_POSTGRES,
+  dbPort: process.env.PORT_POSTGRES
+};
