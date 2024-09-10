018effb94e744e8a8ff8f4a801553287dbd486f0
HectorGarcia01 <hg2779948@gmail.com>
2024-09-10 02:08:30 -0600

    Creación del modelo user

---
+++ src/db/user.model.js
@@ -0,0 +1,52 @@
+const Datatype = require('faker/lib/datatype');
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const USER_TABLE = 'users';
+
+const UserSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true
+  },
+
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'create_at',
+    defaultVaule: Sequelize.NOW
+  }
+};
+
+class User extends Model {
+  static associate() {
+
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: USER_TABLE,
+      modelName: 'User',
+      timeestamps: false
+    }
+  }
+};
+
+module.exports = {
+  USER_TABLE,
+  UserSchema,
+  User
+};
