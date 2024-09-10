1845e505ce15751885ff619eb4be136d0811f409
HectorGarcia01 <hg2779948@gmail.com>
2024-09-10 01:34:29 -0600

    Modificación de la variable de entorno de nombre de usuario de la bd

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3000,
-  dbUser: process.env.DB_POSTGRES,
+  dbUser: process.env.USER_POSTGRES,
   dbPassword: process.env.PASSWORD_POSTGRES,
   dbHost: process.env.HOST_POSTGRES,
   dbName: process.env.DB_POSTGRES,
