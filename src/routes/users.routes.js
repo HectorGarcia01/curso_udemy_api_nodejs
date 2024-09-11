4253fc4d9d72b48c51864fd315f5264b7abb75bf
HectorGarcia01 <hg2779948@gmail.com>
2024-09-11 16:36:42 -0600

    Configuración de los endpoints para la creación del usuario, obtener, actualizar y eliminar usuario por id

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const {
-  getAllUsers
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser
 } = require('../services/servicesUsers');
+const validatorHandler = require('../middlewares/validator.handler');
+const {
+  schemaUserCreate,
+  schemaUserUpdate,
+  schemaGetUser
+} = require('../schemas/schemaUser');
+
 const router = express.Router();
 
 router.get('/', getAllUsers);
+router.get('/:id', validatorHandler(schemaGetUser, 'params'), getUser);
+router.post('/', validatorHandler(schemaUserCreate, 'body'), createUser);
+router.patch('/:id',
+  validatorHandler(schemaGetUser, 'params'), //Para evaluar si envía un id
+  validatorHandler(schemaUserUpdate, 'body'), //Para evaluar los campos enviados
+  updateUser
+);
+router.delete('/:id', validatorHandler(schemaGetUser, 'params'), deleteUser);
 
 module.exports = router;
