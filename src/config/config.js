5f9898ab71fa9af1506a37647aab9350ee85509d
HectorGarcia01 <hg2779948@gmail.com>
2024-09-11 17:29:06 -0600

    Cambio de nombres de las variables de entorno para la base de datos

--- src/config/config.js
+++ src/config/config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3000,
-  dbUser: process.env.USER_POSTGRES,
-  dbPassword: process.env.PASSWORD_POSTGRES,
-  dbHost: process.env.HOST_POSTGRES,
-  dbName: process.env.DB_POSTGRES,
+  dbUser: process.env.USER_DB,
+  dbPassword: process.env.PASSWORD_DB,
+  dbHost: process.env.HOST_DB,
+  dbName: process.env.DB_NAME,
   dbPort: process.env.PORT_DB
 };
