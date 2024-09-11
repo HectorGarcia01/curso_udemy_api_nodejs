bf6ded5e7a9f7dd0c555eced4e423b1fb52d3ade
HectorGarcia01 <hg2779948@gmail.com>
2024-09-11 16:20:56 -0600

    Creación de la función para obtener todos los usuarios, crear nuevo usuario, actualizar usuario y eliminar usuario

--- src/services/servicesUsers.js
+++ src/services/servicesUsers.js
@@ -1,16 +1,62 @@
+const { models } = require('../libs/sequelize');
+
 const getAllUsers = async (req, res) => {
-  const { limit, offset } = req.query;
-
-  if (limit && offset) {
-    return res.json({
-      limit,
-      offset
-    });
+  try {
+    const response = await models.User.findAll();
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
+};
 
-  res.send("No hay ningún parámetro");
+const createUser = async (req, res) => {
+  try {
+    const user = await models.User.findOne({ where: { email: req.body.email } });
+    console.log(user);
+    if (user) {
+      return res.status(409).send({ message: "El usuario ya existe!!" });
+    }
+
+    await models.User.create(req.body);
+    res.status(201).send({ message: "Usuario creado con éxito!!" });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const user = await models.User.findByPk(req.params.id);
+
+    if (!user) {
+      return res.status(404).send({ message: "Usuario no encontrado." });
+    }
+
+    await user.update(req.body);
+    res.status(200).send({ message: "Usuario actualizado con éxito!!" });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const user = await models.User.findOne({ where: { id: req.params.id } });
+
+    if (!user) {
+      return res.status(404).send({ message: "Usuario no encontrado." });
+    }
+
+    await user.destroy();
+    res.status(200).send({ message: "Usuario eliminado con éxito!!" });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
 module.exports = {
-  getAllUsers
-}
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser
+};
