8a4ee3e690abd912ecd00888bb1e14f143666f62
HectorGarcia01 <hg2779948@gmail.com>
2024-09-12 17:05:17 -0600

    Se agregó el campo para la llave foránea de categoría y su relación de muchos a uno entre product y category

--- src/db/models/product.model.js
+++ src/db/models/product.model.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes, Sequelize } = require('sequelize');
+const { CATEGORY_TABLE } = require('./category.model');
 
 const PRODUCT_TABLE = 'products';
 
@@ -31,12 +32,25 @@ const ProductSchema = {
     allowNull: false,
     type: DataTypes.DATE,
     defaultValue: Sequelize.NOW
+  },
+
+  categoryIdFk: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    unique: true,
+    references: {
+      model: CATEGORY_TABLE,
+      key: 'id'
+    },
+
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
   }
 };
 
 class Product extends Model {
-  static associate() {
-
+  static associate(models) {
+    this.belongsTo(models.Category, { as: 'Category', foreignKey: 'categoryIdFk' });
   }
 
   static config(sequelize) {
