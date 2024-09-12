453ba9f2049a9073b730c6957d73f2c56b2e603a
HectorGarcia01 <hg2779948@gmail.com>
2024-09-11 22:55:21 -0600

    Eliminación de la conexión con postgres, debido a que se implementó el ORM de sequelize

--- src/libs/postgres.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-const { dbUser, dbPassword, dbHost, dbPort, dbName } = require('../config/config');
-const USER = encodeURIComponent(dbUser);
-const PASSWORD = encodeURIComponent(dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${dbHost}:${dbPort}/${dbName}`;
-
-const pool = new Pool({ connectionString: URI });
-
-module.exports = pool;
