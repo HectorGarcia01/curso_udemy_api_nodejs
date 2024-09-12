c1c524cf22095542f41c479938a6d7602ed2a370
HectorGarcia01 <hg2779948@gmail.com>
2024-09-12 00:06:38 -0600

    Inicialización del modelo client y la asociación respectiva

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,9 +1,13 @@
 const { User, UserSchema } = require('./user.model');
 const { Product, ProductSchema } = require('./product.model');
+const { Client, ClientSchema } = require('./client.model');
 
 const setupModels = (sequelize) => {
   User.init(UserSchema, User.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
+  Client.init(ClientSchema, Client.config(sequelize));
+
+  Client.associate(sequelize.models);
 };
 
 module.exports = setupModels;
