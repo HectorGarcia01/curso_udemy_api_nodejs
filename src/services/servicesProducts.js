6f411bd49ae039d8795023918e4f3571e9e3f967
HectorGarcia01 <hg2779948@gmail.com>
2024-09-11 18:30:42 -0600

    Implementación real para la actualización de un producto por id en la bd

--- src/services/servicesProducts.js
+++ src/services/servicesProducts.js
@@ -45,16 +45,16 @@ const createNewProduct = async (req, res) => {
 
 const updateProduct = async (req, res) => {
   try {
-    const { id } = req.params;
-    const body = req.body;
+    const product = await models.Product.findByPk(req.params.id);
 
-    res.json({
-      message: "Success",
-      Product: body,
-      id
-    });
+    if (!product) {
+      return res.status(404).send({ message: "Producto no encontrado." });
+    }
+
+    await product.update(req.body);
+    res.status(200).send({ message: "Producto actualizado con éxito." });
   } catch (error) {
-    console.log(error);
+    res.status(500).send(error.message);
   }
 };
 
